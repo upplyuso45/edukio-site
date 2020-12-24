d795845ac8a85a1e9afbf618825777252659cf25
GuiEnrik Note <guienrik@gmail.com>
2020-12-24 01:53:09 -0300

    Edit

--- js/new-age.js
+++ js/new-age.js
@@ -30,8 +30,10 @@
   var navbarCollapse = function() {
     if ($("#mainNav").offset().top > 100) {
       $("#mainNav").addClass("navbar-shrink");
+      $("#mainNav").addClass("logo-nav");
     } else {
       $("#mainNav").removeClass("navbar-shrink");
+      $("#mainNav").removeClass("logo-nav");
     }
   };
   // Collapse now if page is not at top
