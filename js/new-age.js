9ac264f4da0a1b58ddbccd0809b6b508368ca46d
GuiEnrik Note <guienrik@gmail.com>
2021-03-26 14:13:10 -0300

    Incluida sessão ex-colaboradores, ano dinâmico

--- js/new-age.js
+++ js/new-age.js
@@ -46,3 +46,12 @@
   $(window).scroll(navbarCollapse);
 
 })(jQuery); // End of use strict
+
+
+$('.panel-collapse').on('show.bs.collapse', function () {
+  $(this).siblings('.panel-heading').addClass('active');
+});
+
+$('.panel-collapse').on('hide.bs.collapse', function () {
+  $(this).siblings('.panel-heading').removeClass('active');
+});
